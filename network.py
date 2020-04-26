1d13db04ab76f230f3dd726658c5da4d5c549ca3
Emma Ling <emmaling27@gmail.com>
2020-04-26 09:53:37 -0700

    refactored network code

--- network.py
+++ network.py
@@ -3,53 +3,57 @@ import numpy as np
 
 
 class Network():
-    """
-    Generate a network using Toivonen et al.'s social network model
-    """
+    """ Generate a network using Toivonen et al.'s social network model """
 
-    def __init__(self, n_0, n, m_r=None, m_s=None):
+    def __init__(self, n_0, n):
         """
         n_0: initial seed of nodes
         n: number of nodes the network should have
-        m_r: mean of initial contacts to connect a new node to
-        m_s: mean of secondary contacts (friends of friends)
         """
+        assert(n_0 <= n)
+        assert(n_0 > 0)
         self.n_0 = n_0
         self.n = n
-        self.m_r = m_r
-        self.m_s = m_s
         self.ic_rate = .016
         self.icf_rate = .08
         self.close_friends_rate = .1
+        self.g = nx.Graph()
+        self.rng = np.random.default_rng()
+
+    def add_node(self, i):
+        """ Adds node i to graph """
+        self.g.add_node(i,
+            senior=self.rng.binomial(1, .5),
+            ic=self.rng.binomial(1, self.ic_rate),
+            icf=self.rng.binomial(1, self.icf_rate))
+
+    def add_edge(self, i, j):
+        """ Adds edge (i, j) to graph """
+        self.g.add_edge(i,
+            j,
+            close=self.rng.binomial(1, self.close_friends_rate))
 
     def generate_network(self):
-        g = nx.Graph()
-        rng = np.random.default_rng()
-        g.add_nodes_from(range(self.n_0))
+        """
+        Generates the network using Toivonen et al.'s social network model
+        """
+        for node in range(self.n_0):
+            self.add_node(node)
         for new_node in range(self.n_0, self.n):
-            g.add_node(new_node,
-                senior=rng.binomial(1, .5),
-                ic=rng.binomial(1, self.ic_rate),
-                icf=rng.binomial(1, self.icf_rate))
-            c1 = rng.binomial(1, .05) + 1
-            initial_contacts = rng.choice(
-                np.array(g.nodes()),
-                c1,
+            self.add_node(new_node)
+            initial_contacts = self.rng.choice(
+                np.array(self.g.nodes()),
+                self.rng.binomial(1, .05) + 1,
                 replace=False)
             secondary_pool = []
             for i in initial_contacts:
-                secondary_pool += list(g.neighbors(i))
-                g.add_edge(new_node,
-                    i,
-                    close=rng.binomial(1, self.close_friends_rate))
-            c2 = min(rng.integers(1, 4), len(secondary_pool))
+                secondary_pool += list(self.g.neighbors(i))
+                self.add_edge(new_node, i)
             if secondary_pool:
-                secondary_contacts = rng.choice(
+                secondary_contacts = self.rng.choice(
                     secondary_pool,
-                    c2,
+                    min(self.rng.integers(1, 4), len(secondary_pool)),
                     replace=False)
                 for i in secondary_contacts:
-                    g.add_edge(new_node,
-                        i,
-                        close=rng.binomial(1, self.close_friends_rate))
-        return g
+                    self.add_edge(new_node, i)
+        return self.g
